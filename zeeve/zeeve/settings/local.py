2aa148e16d4ee6c3925357a1684e234b13a94cc6
Ricktter <ricktter@gmail.com>
2016-09-05 15:38:24 -0500

    first

---
+++ zeeve/zeeve/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'zeeve_db',
+        'USER': 'zeeve_admin',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_URL = '/static/'
